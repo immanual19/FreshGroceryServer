d212fd2f643df9d6eebac634a8c4a7f9b7a2a9b2
immanual19 <immanuelsarker1@gmail.com>
2021-03-31 05:27:53 +0600

    enoughToday

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser=require('body-parser');
 const cors=require('cors');
 const port = process.env.PORT || 8080
 const MongoClient = require('mongodb').MongoClient;
+const { ObjectId } = require('bson');
 const uri = "mongodb+srv://freshGroceryAdmin:x4nd3r1sm3@cluster0.q17pz.mongodb.net/freshGroceryDB?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 require('dotenv').config();
@@ -16,6 +17,7 @@ app.use(cors());
 
 client.connect(err => {
   const productCollection = client.db("freshGroceryDB").collection("products");
+  const orderCollection = client.db("freshGroceryDB").collection("orders");
   // adding product to database starts here
   app.post('/addProduct',(req,res)=>{
     const product=req.body;
@@ -36,7 +38,34 @@ client.connect(err => {
     })
   })
 
+  app.get('/singleproduct/:pdId',(req,res)=>{
+    productCollection.find({_id:ObjectId(req.params.pdId)})
+    .toArray((err,documents)=>{
+      res.send(documents[0]);
+    })
+  })
+
   //fetching all the data from database ends here
+
+  //Oder storing in database starts here
+  app.post('/processorder',(req,res)=>{
+    const order=req.body;
+    orderCollection.insertOne(order)
+    .then(result=>{
+      res.send(result.insertedCount>0)
+    })
+  })
+  //Oder storing in database ends here
+
+  //my order history starts here
+  app.get('/myorderhistory/:myEmailId',(req,res)=>{
+    const emailId=req.params.myEmailId;
+    orderCollection.find({customerEmail:emailId})
+    .toArray((err,documents)=>{
+      res.send(documents);
+    })
+  })
+  //my order history ends here
   
 });
 app.get('/', (req, res) => {
